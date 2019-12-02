7902e9d4475da927918798659e05dba9b2fd7771
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-02 23:29:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  # some_hash_four = {
-  #   key => value
-  # }
+  some_hash_four = {
+    key => value
+  }
 end
 
 def read_from_hash(hash, key)
